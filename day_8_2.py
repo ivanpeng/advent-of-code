09aade47be7f316bfc935baa7f3c6ccf9fda1e95
Ivan Peng <ivanpeng13@gmail.com>
2017-12-08 00:25:55 -0500

    Day 8 files of concern.

---
+++ day_8_2.py
@@ -0,0 +1,48 @@
+import operator
+
+def main(input):
+    registers = {}
+    max_ever = 0
+    for line in input:
+        # parse line
+        key, is_inc, inc_amount, _, if_key, comparator, if_amount = line.split(" ")
+        inc_amount = int(inc_amount)
+        if_amount = int(if_amount)
+        inc_multiplier = 1 if is_inc == "inc" else -1
+        if key not in registers:
+            registers[key] = 0
+        if if_key not in registers:
+            registers[if_key] = 0
+        # Figure out sum now
+        # Determine if the condition is true
+        condition = False
+        if comparator == ">":
+            condition = registers[if_key] > if_amount
+        elif comparator == "<":
+            condition = registers[if_key] < if_amount
+        elif comparator == "!=":
+            condition = registers[if_key] != if_amount
+        elif comparator == ">=":
+            condition = registers[if_key] >= if_amount
+        elif comparator == "<=":
+            condition = registers[if_key] <= if_amount
+        elif comparator == "==":
+            condition = registers[if_key] == if_amount
+        if condition is True:
+            registers[key] += inc_multiplier * (inc_amount)
+            if (registers[key] > max_ever):
+                max_ever = registers[key]
+    print(registers)
+    max_key = max(registers.items(), key=operator.itemgetter(1))[0]
+    print(max_key, registers[max_key])
+    print(max_ever)
+
+
+if __name__ == '__main__':
+    # input = ["b inc 5 if a > 1", "a inc 1 if b < 5", "c dec -10 if a >= 1", "c inc -20 if c == 10"]
+    # main(input)
+    input = []
+    with open("day_8.txt", 'r') as f:
+        for line in f:
+            input.append(line.strip())
+        main(input)
